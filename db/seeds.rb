9ee0ce2f4f3c0c77727dc5cfbce1360f37993507
Dominic Staendel <dominic.staendel@hotmail.de>
2024-02-21 16:47:44 +0100

    new rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Restaurant.destroy_all
+
+restaurants = [
+  { name: "Chinatown", address: "Berlin", phone_number: "01 23 45 67 89", category: "chinese" },
+  { name: "Pizza Plaza", address: "Berlin", phone_number: "06 12 34 56 78", category: "italian" },
+  { name: "Sushi Goku", address: "Cologne", phone_number: "03 12 34 76 78", category: "japanese" },
+  { name: "Le Baguette", address: "Leipzig", phone_number: "01 98 76 54 32", category: "french" },
+  { name: "Frites Forever", address: "Hamburg", phone_number: "02 98 76 54 32", category: "belgian" }
+]
+
+restaurants.each do |restaurant|
+  Restaurant.create!(restaurant)
+end
+
+puts "Created #{Restaurant.count} restaurants."
